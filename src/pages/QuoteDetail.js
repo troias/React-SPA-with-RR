42da2dd3f1e13f2979e068a029eb51140b283e7f
troy <hunter007nz@gmail.com>
2021-09-07 16:22:44 +1200

    base app complete

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { useParams, Route, Link } from 'react-router-dom'
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom'
 import Comments from '../components/comments/Comments'
 import HighlightedQuote from '../components/quotes/HighlightedQuote'
 
 const QuoteDetail = (props) => {
-        //  console.log("QuoteState", typeof props.quoteDetail[0].id)
-
+    //  console.log("QuoteState", typeof props.quoteDetail[0].id)
+    const match = useRouteMatch()
     const params = useParams()
     const paramsId = +params.quoteID
     const quote = props.quoteDetail.find(x => x.id === paramsId)
 
-    console.log("quote", quote)
- 
-  
+    console.log("match", match)
 
- 
+
+
+
 
     if (!quote) {
         return <p>No quote found</p>
@@ -22,13 +22,20 @@ const QuoteDetail = (props) => {
 
     return (
         <div>
-            <Link to={`/quotes/${params.quoteID}/comments`} >
-               
-                <HighlightedQuote text={quote.text} author={quote.author} />
-             
-                </Link>
-      
-            <Route path={`/quotes/${params.quoteID}/comments`}>
+
+            <HighlightedQuote text={quote.text} author={quote.author} />
+
+            <Route path={`${match.path}`} exact>
+                <div className="centered">
+
+                    <Link to={`${match.path}/comments`} className="btn--flat" >
+                        Load Comments
+                    </Link>
+                </div>
+            </Route>
+
+
+            <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
         </div>
