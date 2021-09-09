78767c14f35db2aa91d7d419dbb19d083ce8c60e
troy <hunter007nz@gmail.com>
2021-09-09 18:20:31 +1200

    added build

---
+++ build/static/js/6.f084a514.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[6],{52:function(e,t,c){"use strict";c.r(t);c(0);var n=c(1);t.default=function(){return Object(n.jsx)("div",{className:"centered",children:Object(n.jsx)("p",{children:" Not Found "})})}}}]);
+//# sourceMappingURL=6.f084a514.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,71 +1,58 @@
-import { useEffect, useCallback } from 'react'
-import {
-  Switch,
-  Route,
-  Redirect,
- 
-} from "react-router-dom";
-import Layout from './components/layout/Layout'
+import React, { useEffect, useCallback, Suspense } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import MainNavigation from "./components/layout/MainNavigation";
-import useHttp from './hooks/use-http'
-import AllQuotes from './pages/AllQuotes'
-import NewQuote from './pages/NewQuote'
-import QuoteDetail from "./pages/QuoteDetail";
-import NotFound from './pages/NotFound';
-import {  getAllQuotes } from './lib/api'
+import useHttp from "./hooks/use-http";
+import { getAllQuotes } from "./lib/api";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
 
 function App() {
+  const { sendRequest, status, data: loadedQuotes } = useHttp(getAllQuotes);
 
-
-  const { sendRequest, status, data: loadedQuotes } = useHttp(getAllQuotes)
-
-
-  const pullAllQuotes =  useCallback(() => {
-    sendRequest()
-  }, [sendRequest])
-
+  const pullAllQuotes = useCallback(() => {
+    sendRequest();
+  }, [sendRequest]);
 
   useEffect(() => {
-    pullAllQuotes()
-    return () => {
-
-    };
-
-  }, [pullAllQuotes])
-
-
-
-
-
-
+    pullAllQuotes();
+    return () => {};
+  }, [pullAllQuotes]);
 
   return (
     <>
       <MainNavigation />
-
       <Layout>
-        <Switch>
-
-          <Route exact path="/">
-            <Redirect to="/quotes" />
-          </Route>
-          <Route exact path="/quotes" >
-            <AllQuotes quotes={loadedQuotes} status={status} />
-          </Route>
-          <Route path="/quotes/:quoteID" >
-            <QuoteDetail />
-          </Route>
-          <Route path="/add-quote">
-            <NewQuote />
-          </Route>
-
-          <Route path='*'>
-            <NotFound />
-          </Route>
-        </Switch>
+        <Suspense
+          fallback={
+            <div>
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/quotes" />
+            </Route>
+            <Route exact path="/quotes">
+              <AllQuotes quotes={loadedQuotes} status={status} />
+            </Route>
+            <Route path="/quotes/:quoteID">
+              <QuoteDetail />
+            </Route>
+            <Route path="/add-quote">
+              <NewQuote />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
-
     </>
   );
 }
